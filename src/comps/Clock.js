5924e15c031e3850234191e1ed6855277d85e6c6
Daniel <beta@ozx.me>
2022-09-27 21:15:23 +0000

    Fixed states

--- src/comps/Clock.js
+++ src/comps/Clock.js
@@ -1,11 +1,25 @@
+import React from 'react';
+import '../styles/Player.css';
+
 export default function Clock() {    
-    const date = new Date();
-    var hour = date.getHours();
-    var min = date.getMinutes();
+    const [date, setDate] = React.useState(new Date());
+
+    React.useEffect(() => {
+        const timerID = setInterval(() => tick(), 1000);
+        
+        return function cleanup() {
+        clearInterval(timerID);
+        };
+        
+        });
+        
+        function tick() {
+        setDate(new Date());
+    }
 
     return (
         <div className="Clock">
-            {hour}:{min}
+            {date.getHours()}:{date.getMinutes()}
         </div>
     )
 }
--- src/comps/Player.js
+++ src/comps/Player.js
@@ -1,4 +1,26 @@
-export default function Player(props) {
+import React from 'react';
+import '../styles/Player.css';
+
+export default function Player() {
+    const [date, setDate] = React.useState(new Date());
+
+    React.useEffect(() => {
+        const timerID = setInterval(() => tick(), 1000);
+        
+        return function cleanup() {
+        clearInterval(timerID);
+        };
+        
+        });
+        
+        function tick() {
+        setDate(new Date());
+    }
+
+    var base = 'https://cdn.ozx.me/ac'
+    var game = 'new-horizons'
+    var weather = 'clear'
+
     function playAudio() {
         var audioPlayer = document.getElementById('audio');
         
@@ -12,11 +34,11 @@ export default function Player(props) {
     return (
         <div className="Player">
             <audio id="audio" controls="" loop> 
-                <source id="oggSource" src={props.url} type="audio/ogg"></source>
+                <source id="oggSource" src={`${base}/${game}/${weather}/${date.getHours()}.ogg`} type="audio/ogg"></source>
             </audio>
 
             <button className="mediaControl" onClick={playAudio}>
-                Play
+                A
             </button>
         </div>
     );
