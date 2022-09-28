7dbe8576655797931c18dde680f849d4e451f3df
Daniel <beta@ozx.me>
2022-09-27 21:02:10 -0700

    Fixed clock

--- src/comps/Clock.js
+++ src/comps/Clock.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import '../styles/Player.css';
+import React from "react";
+import "../styles/Clock.css";
 
-export default function Clock() {    
-    const [date, setDate] = React.useState(new Date());
+export default function Clock() {
+  const [date, setDate] = React.useState(new Date());
 
-    React.useEffect(() => {
-        const timerID = setInterval(() => tick(), 1000);
-        
-        return function cleanup() {
-        clearInterval(timerID);
-        };
-        
-        });
-        
-        function tick() {
-        setDate(new Date());
-    }
+  React.useEffect(() => {
+    const timerID = setInterval(() => tick(), 1000);
 
-    return (
-        <div className="Clock">
-            {date.getHours()}:{date.getMinutes()}
-        </div>
-    )
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  });
+
+  function tick() {
+    setDate(new Date());
+  }
+
+  return (
+    <div className="Clock">
+      {date.getHours() > 12 ? date.getHours() - 12 : date.getHours()}
+      {date.getSeconds() % 2 == 0 ? ":" : " "}
+      {date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()}
+    </div>
+  );
 }
