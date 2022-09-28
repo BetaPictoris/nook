c58edc8e9da724f166446e42c2255b3f410d8212
Daniel <beta@ozx.me>
2022-09-27 20:51:49 -0700

    Player: Reformat and fix CDN URL

--- src/comps/Player.js
+++ src/comps/Player.js
@@ -1,45 +1,48 @@
-import React from 'react';
-import '../styles/Player.css';
+import React from "react";
+import "../styles/Player.css";
 
 export default function Player() {
-    const [date, setDate] = React.useState(new Date());
+  const [date, setDate] = React.useState(new Date());
 
-    React.useEffect(() => {
-        const timerID = setInterval(() => tick(), 1000);
-        
-        return function cleanup() {
-        clearInterval(timerID);
-        };
-        
-        });
-        
-        function tick() {
-        setDate(new Date());
+  React.useEffect(() => {
+    const timerID = setInterval(() => tick(), 1000);
+
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  });
+
+  function tick() {
+    setDate(new Date());
+  }
+
+  var base = "https://cdn.ozx.me/ac";
+  var game = "new-horizons";
+  var weather = "clear";
+
+  function playAudio() {
+    var audioPlayer = document.getElementById("audio");
+
+    if (audioPlayer.paused) {
+      audioPlayer.play();
+    } else {
+      audioPlayer.pause();
     }
+  }
 
-    var base = 'https://cdn.ozx.me/ac'
-    var game = 'new-horizons'
-    var weather = 'clear'
+  return (
+    <div className="Player">
+      <audio id="audio" controls="" loop>
+        <source
+          id="oggSource"
+          src={`${base}/${game}/music/${weather}/${date.getHours()}.ogg`}
+          type="audio/ogg"
+        ></source>
+      </audio>
 
-    function playAudio() {
-        var audioPlayer = document.getElementById('audio');
-        
-        if (audioPlayer.paused) {
-            audioPlayer.play()
-        } else {
-            audioPlayer.pause()
-        }
-    }
-
-    return (
-        <div className="Player">
-            <audio id="audio" controls="" loop> 
-                <source id="oggSource" src={`${base}/${game}/${weather}/${date.getHours()}.ogg`} type="audio/ogg"></source>
-            </audio>
-
-            <button className="mediaControl" onClick={playAudio}>
-                A
-            </button>
-        </div>
-    );
+      <button className="mediaControl" onClick={playAudio}>
+        A
+      </button>
+    </div>
+  );
 }
