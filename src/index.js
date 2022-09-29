786968f860603d5a2dccc714ace60335656977e7
beta <beta@ozx.me>
2022-09-28 21:18:32 -0700

    Setup local storage

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./App";
 
+// Set local storage, if it hasn't been set.
+if (!sessionStorage.getItem("game")) {
+  sessionStorage.setItem("game", "new-horizons")
+  sessionStorage.setItem("weather", "clear")
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
