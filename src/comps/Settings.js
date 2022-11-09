1b8605bc83781443851a88e0bdb7d2fffe225dc4
Daniel <beta@ozx.me>
2022-11-09 21:34:32 +0000

    Remove unused import (fixes JS-C1003)

--- src/comps/Settings.js
+++ src/comps/Settings.js
@@ -1,5 +1,3 @@
-import * as React from "react";
-
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
