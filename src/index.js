1ecbd06bc3528d8102cd71e99c7e7745d23b4481
BetaPictoris <beta@ozx.me>
2022-12-20 16:51:39 -0800

    Preload background image

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ import "./styles/index.css";
 
 import App from "./App";
 
+// Images to preload
+const images = ["//cdn.ozx.me//ac/pattern_fallback.png"];
+for (const i in images) {
+  var image = new Image();
+  image.src = images[i];
+}
+
 // Set local storage, if it hasn't been set.
 if (!sessionStorage.getItem("game")) {
   sessionStorage.setItem("game", "new-horizons");
